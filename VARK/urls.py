c870d6ffdffdfc2955d81f7674ed9fc97bff661f
AlanRA6 <deprueba761@gmail.com>
2025-11-23 23:50:46 -0600

    act dashboard 1

--- VARK/urls.py
+++ VARK/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
-from .views import vark_results, vark_test
+from .views import vark_results, vark_test, dashboard_vark, dashboard_data
 
 urlpatterns = [
     path('vark_test/', vark_test, name='vark_test'),
     path('vark_results/', vark_results, name='vark_results'),
+    path('vark_dashboard/', dashboard_vark, name='vark_dashboard'),
+    path('dashboard-data/', dashboard_data, name='dashboard_data')
 ]
